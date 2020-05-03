6338463da86ff459300c55c16f66f300b1c0b835
gbarbieru <gbarbieru@bitdefender.com>
2020-05-03 14:26:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+The scope of this repository is to collect USB HID list NZXT Devices.
+Feel free to contribute to this repo with a pull-request.
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    "vendorId": 7793,
+    "productId": 12296,
+    "product": "NZXT KrakenZ Device"
+  },
+  {
+    "vendorId": 30611,
+    "productId": 22802,
+    "product": "NZXT E-Series PSU"
+  },
+  {
+    "vendorId": 7793,
+    "productId": 8194,
+    "product": "NZXT USB Device"
+  },
+  {
+    "vendorId": 7793,
+    "productId": 8198,
+    "product": "NZXT USB Device"
+  }
+];
